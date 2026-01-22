5c8984e2bb4743ae704e1211c03c5e040f3f57dc
MegaITA <aleberto@live.it>
2026-01-22 12:26:24 +0100

    Add transition wrappers (FadeIn, ZoomIn, SlideIn)

---
+++ src/components/transitions/FadeIn.vue
@@ -0,0 +1,31 @@
+<template>
+  <Transition name="fade-in">
+    <slot></slot>
+  </Transition>
+</template>
+
+<script setup>
+defineProps({
+  duration: {
+    type: Number,
+    default: 300
+  }
+})
+</script>
+
+<style scoped>
+.fade-in-enter-active,
+.fade-in-leave-active {
+  transition: opacity v-bind(duration + 'ms') ease-out;
+}
+
+.fade-in-enter-from,
+.fade-in-leave-to {
+  opacity: 0;
+}
+
+.fade-in-enter-to,
+.fade-in-leave-from {
+  opacity: 1;
+}
+</style>
---
+++ src/components/transitions/ZoomIn.vue
@@ -0,0 +1,33 @@
+<template>
+  <Transition name="zoom-in">
+    <slot></slot>
+  </Transition>
+</template>
+
+<script setup>
+defineProps({
+  duration: {
+    type: Number,
+    default: 300
+  }
+})
+</script>
+
+<style scoped>
+.zoom-in-enter-active,
+.zoom-in-leave-active {
+  transition: all v-bind(duration + 'ms') ease-out;
+}
+
+.zoom-in-enter-from,
+.zoom-in-leave-to {
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.zoom-in-enter-to,
+.zoom-in-leave-from {
+  opacity: 1;
+  transform: scale(1);
+}
+</style>
